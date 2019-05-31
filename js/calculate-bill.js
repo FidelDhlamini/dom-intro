62005a5c1eff16d1d51a44cc9fcc07cffe510a12
FidelDhlamini <goodkidfidel@gmail.com>
2019-05-31 12:58:32 +0200

    button works now

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,7 +1,34 @@
 //get a reference to the calculate button
+var billStringField = document.querySelector(".billString");
+var calculateBtn = document.querySelector(".calculateBtn");
+var billTotalElement = document.querySelector(".billTotal");
+
+function calculateBtnClicked(){
+    // get the string entered in the textArea
+    var billString = billStringField.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
+    
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
+calculateBtn.addEventListener('click', calculateBtnClicked);
 
 //get a reference to the billTotal element
-
+    // logic goes here
 //get a reference to the billString
 
 //create the function that will be called when the calculate button is pressed
